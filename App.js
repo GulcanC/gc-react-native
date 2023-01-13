42920e90efe8b45ee3f5c6428053cc38e80ac999
Gulcan COSKUN <gulcancoskun.a@gmail.com>
2023-01-13 11:31:21 +0100

    create input text and button

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 
+// text and wiev are core compnents
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+
+// regular functioanal component,  App is root component
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Gulcan COSKUN!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your skills!" />
+        <Button title="Add Skill" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of skills...</Text>
+      </View>
     </View>
   );
 }
 
+// style sheet object
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    backgroundColor: "",
     flex: 1,
-    backgroundColor: "#fff",
+    padding: 50,
+    paddingHorizontal: 16,
+  },
+
+  inputContainer: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "blue",
+  },
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: "blue",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });
